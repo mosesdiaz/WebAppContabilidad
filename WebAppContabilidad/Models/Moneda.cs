68a68485e42c6fe79ed20d7343a1e1db39516e35
mosesdiaz <moisesmldiaz18@gmail.com>
2021-07-15 18:20:03 -0400

    Add project files.

---
+++ WebAppContabilidad/Models/Moneda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppContabilidad.Models
+{
+    public class Moneda
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string TasaDeCambio { get; set; }
+    }
+}
---
+++ WebAppContabilidad/Models/TipoDeCuenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppContabilidad.Models
+{
+    public enum Origen
+    {
+        DB, CR
+    }
+    public class TipoDeCuenta
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public Origen Origen { get; set; }
+    }
+}
